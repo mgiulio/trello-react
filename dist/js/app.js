3d5a05c1fa5121790ca5c890a0f8776e9d5f0cbe
mgiulio <giulio.mainardi@gmail.com>
2015-07-17 14:43:49 +0200

    Topbar: dropdowns: toggle visibility via class toggling

--- dist/js/app.js
+++ dist/js/app.js
@@ -40,32 +40,48 @@ var
                <div className="section misc">
                   <div className="create">
                      <button className="add-new">+</button>
+                     <nav className="menu">
+                        <ul className="items">
+                           <li className="item"><a href="#">Create Board</a></li>
+                           <li className="item"><a href="#">Create Personal Organization</a></li>
+                           <li className="item"><a href="#">Create Business Organization</a></li>
+                        </ul>
+                     </nav>
                   </div>
                   <div className="user">
-                     <label htmlFor="toggle-user-menu" className="button">
+                     <button className="button">
                         <img className="avatar" src={this.props.user.avatarUrl} alt="" />
                         <span className="username">{this.props.user.username}</span>
-                     </label>
-                     <input type="checkbox" id="toggle-user-menu" />
-                     <nav className="user__menu">
-                        <ul className="user__menu__items">
-                           <li className="user__menu__items__item"><a href="#">Profile</a></li>
-                           <li className="user__menu__items__item"><a href="#">Cards</a></li>
-                           <li className="user__menu__items__item"><a href="#">Billing</a></li>
-                           <li className="user__menu__items__item last-of-group"><a href="#">Settings</a></li>
-                           <li className="user__menu__items__item"><a href="#">Help</a></li>
-                           <li className="user__menu__items__item"><a href="#">Apps</a></li>
-                           <li className="user__menu__items__item"><a href="#">Shortcuts</a></li>
-                           <li className="user__menu__items__item last-of-group"><a href="#">Share Trello(for free Trello Gold)</a></li>
-                           <li className="user__menu__items__item"><a href="#">Getting Started Guide</a></li>
-                           <li className="user__menu__items__item last-of-group"><a href="#">Trello Blog</a></li>
-                           <li className="user__menu__items__item last-of-group"><a href="#">Change Language</a></li>
-                           <li className="user__menu__items__item"><a href="#">Log Out</a></li>
+                     </button>
+                     <nav className="menu">
+                        <ul className="items">
+                           <li className="item"><a href="#">Profile</a></li>
+                           <li className="item"><a href="#">Cards</a></li>
+                           <li className="item"><a href="#">Billing</a></li>
+                           <li className="item last-of-group"><a href="#">Settings</a></li>
+                           <li className="item"><a href="#">Help</a></li>
+                           <li className="item"><a href="#">Apps</a></li>
+                           <li className="item"><a href="#">Shortcuts</a></li>
+                           <li className="item last-of-group"><a href="#">Share Trello(for free Trello Gold)</a></li>
+                           <li className="item"><a href="#">Getting Started Guide</a></li>
+                           <li className="item last-of-group"><a href="#">Trello Blog</a></li>
+                           <li className="item last-of-group"><a href="#">Change Language</a></li>
+                           <li className="item"><a href="#">Log Out</a></li>
                         </ul>
                      </nav>
                   </div>
                   <div className="notifications">
-                     <button>N</button>
+                     <button class="button">+</button>
+                     <nav className="menu">
+                        <ul className="items">
+                           <li className="item"><a href="#">Notification #1</a></li>
+                           <li className="item"><a href="#">Notification #2</a></li>
+                           <li className="item"><a href="#">Notification #3</a></li>
+                           <li className="item"><a href="#">Notification #4</a></li>
+                           <li className="item"><a href="#">Notification #5</a></li>
+                           <li className="item"><a href="#">Notification #6</a></li>
+                        </ul>
+                     </nav>
                   </div>
                </div>
                <a className="logo" href="/">Trello</a>
