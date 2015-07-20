c975c8370a9082e717406a7399d1690e62fdf1dc
mgiulio <giulio.mainardi@gmail.com>
2015-07-20 15:19:59 +0200

    Segregate Trello API code in an object

--- dist/js/app.js
+++ dist/js/app.js
@@ -9,39 +9,13 @@ var App = React.createClass({
    componentDidMount: function() {
       console.log('start activity indicator');
 
-      var
-         url = 'https://api.trello.com/1/boards/' + this.props.boardId + '/?key=' + this.props.appKey
+      trelloAPI.getBoard(this.props.boardId)
+         .then(function(processedBoard) { return {board: processedBoard}; })
+         .then(this.setState.bind(this))
+         .catch(function() { console.log(arguments); })
       ;
 
-      fetch(url)
-         .then(
-            function(response) {
-               if (response.status != 200)
-                  throw new Error('Status error code: ' + response.status );
-
-               response.json()
-                  .then(this.processBoardJSON)
-                  .then(function(processedBoard) { return {board: processedBoard}; })
-                  .then(this.setState.bind(this))
-                  .catch(function() {
-                     throw new Error('Cannot parse the board JSON');
-                  })
-               ;
-            }.bind(this),
-            function() {
-               throw new Error('Connection error');
-            }
-      );
       console.log('stop activity indicator');
-   },
-
-   processBoardJSON: function(b) {
-      return {
-         id: b.id,
-         name: b.name,
-         url: b.url,
-         shortUrl: b.shortUrl
-      };
    },
 
    render: function() {
---
+++ src/js/trelloAPI.js
@@ -0,0 +1,51 @@
+var trelloAPI = (function() {
+
+   var
+      appKey
+   ;
+
+   function getBoard(id) {
+      var
+         url = 'https://api.trello.com/1/boards/' + id + '/?key=' + appKey
+      ;
+
+      return new Promise(function(resolve, reject) {
+         fetch(url)
+            .then(
+               function(response) {
+                  if (response.status != 200)
+                     reject('Status error code: ' + response.status );
+
+                  response.json()
+                     .then(
+                        function(boardJSON) { resolve(processBoardJSON(boardJSON)); },
+                        function() { reject('Cannot parse the board JSON'); }
+                     );
+               },
+               function() {
+                  reject('Connection error');
+               }
+         );
+      });
+
+   }
+
+   function processBoardJSON(b) {
+      return {
+         id: b.id,
+         name: b.name,
+         url: b.url,
+         shortUrl: b.shortUrl
+      };
+   }
+
+   function setKeys(k) {
+      appKey = k.appKey;
+   }
+
+   return {
+      setKeys: setKeys,
+      getBoard: getBoard
+   };
+
+})();
