730f440193e431475484348003f912d4d9b924a1
mgiulio <giulio.mainardi@gmail.com>
2015-08-07 17:08:30 +0200

    public boards model

---
+++ src/js/public-boards.js
@@ -0,0 +1,19 @@
+var publicBoards = [
+   {
+      catname:  'Trello Development',
+      boards: [
+         {
+            name: 'board name',
+            url:
+            desc: // full original desc, let the view(Welcome make the eventual excerpt)
+            <li className="boards-section__items__item"><a href="/board/cI66RoQS">Trello Public API</a></li>
+            <li className="boards-section__items__item"><a href="/board/mRn3F1pT">Trello Development - Shipped!</a></li>
+            <li className="boards-section__items__item"><a href="/board/dpX2j6lT">Open Source Libraries</a></li>
+            <li className="boards-section__items__item"><a href="/board/5tj4qAvo">Trello iOS App</a></li>
+            <li className="boards-section__items__item"><a href="/board/nPNSBZjB">Trello Resources</a></li>
+         }
+      ]
+   }
+]
+
+module.exports = publicBoards;
