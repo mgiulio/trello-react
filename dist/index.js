03f461b23209a9651e7046a2cdcd4ef46965f889
mgiulio <giulio.mainardi@gmail.com>
2015-08-19 12:44:09 +0200

    Setup Node.js backend

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
