5149ba49e60ba8fca6f5b6f5c85e005d020ce2db
mgiulio <giulio.mainardi@gmail.com>
2015-08-26 17:10:41 +0200

    CardPage: Basic CSS

--- dist/public/js/bundle.js
+++ dist/public/js/bundle.js
@@ -23314,44 +23314,46 @@ var CardPage = React.createClass({displayName: "CardPage",
       var comments = [
          {
             username: 'user-00',
-            avatarUrl: 'foo.png',
+            avatarUrl: '/img/avatar.png',
             commentText: "@hostilefork Interesting thought, I'll pass that on! For now, you might want to manually add a visual indicator to the card at the top of the list to make that more apparent(e.g. attach a pic and use as card cover)",
             timestamp: 'Aug 12 at 6:56 PM'
          },
          {
             username: 'user-00',
-            avatarUrl: 'foo.png',
+            avatarUrl: '/img/avatar.png',
             commentText: "@hostilefork Interesting thought, I'll pass that on! For now, you might want to manually add a visual indicator to the card at the top of the list to make that more apparent(e.g. attach a pic and use as card cover)",
             timestamp: 'Aug 12 at 6:56 PM'
          },
          {
             username: 'user-01',
-            avatarUrl: 'foo.png',
+            avatarUrl: '/img/avatar.png',
             commentText: "@hostilefork Interesting thought, I'll pass that on! For now, you might want to manually add a visual indicator to the card at the top of the list to make that more apparent(e.g. attach a pic and use as card cover)",
             timestamp: 'Aug 12 at 6:56 PM'
          },
          {
             username: 'user-02',
-            avatarUrl: 'foo.png',
+            avatarUrl: '/img/avatar.png',
             commentText: "@hostilefork Interesting thought, I'll pass that on! For now, you might want to manually add a visual indicator to the card at the top of the list to make that more apparent(e.g. attach a pic and use as card cover)",
             timestamp: 'Aug 12 at 6:56 PM'
          },
          {
             username: 'user-03',
-            avatarUrl: 'foo.png',
+            avatarUrl: '/img/avatar.png',
             commentText: "@hostilefork Interesting thought, I'll pass that on! For now, you might want to manually add a visual indicator to the card at the top of the list to make that more apparent(e.g. attach a pic and use as card cover)",
             timestamp: 'Aug 12 at 6:56 PM'
          }
       ];
 
       var commentItems = comments.map(function(c)  
-         {return React.createElement("li", null, 
-            React.createElement("span", {className: "username"}, c.username), 
+         {return React.createElement("li", {className: "comment"}, 
+            React.createElement("h2", {className: "username"}, c.username), 
             React.createElement("img", {className: "avatar", src: c.avatarUrl}), 
             React.createElement("div", {className: "content"}, 
                c.commentText
             ), 
-            React.createElement("time", null, c.timestamp)
+            React.createElement("p", {className: "meta"}, 
+               React.createElement("time", {className: "timestamp"}, c.timestamp)
+            )
          );}
       );
 
@@ -23361,16 +23363,30 @@ var CardPage = React.createClass({displayName: "CardPage",
             React.createElement("div", {className: "wrap"}, 
                React.createElement("div", {className: "main"}, 
                   React.createElement("header", null, 
-                     React.createElement("h1", {className: "title"}, "Card Title"), 
+                     React.createElement("h1", {className: "title"}, "What can you expect from this board"), 
                      React.createElement("div", {className: "meta"}, 
-                        React.createElement(MetaItem, {icon: "summary", className: "list"}, 
-                           "Foo List"
+                        React.createElement(MetaItem, {icon: "summary", className: "parent-list"}, 
+                           React.createElement("a", {href: "#"}, "Foo List")
                         )
                      )
                   ), 
-                  React.createElement("img", {className: "cover", src: "foo.png"}), 
+                  React.createElement("img", {className: "cover", src: "/img/avatar.png"}), 
                   React.createElement("div", {className: "description"}, 
-                     React.createElement("p", null, "This is the description for card #", this.props.params.id)
+                     React.createElement("p", null, "This is the description for card #", this.props.params.id), 
+                     "Why is this board here?" + ' ' +
+"The primary purpose of this board is to communicate with our customers what the Trello team is working on, what’s potentially coming up, and to listen to the insights our customers have about what they’d like to see in Trello." + ' ' +
+
+"Regular updates to cards that are In Progress" + ' ' +
+"If a card is “In Progress,” that means we’re working on it. You can expect to see regular updates to cards that are on the In Progress list in Trello. The Trello support team will update this list on a regular basis and read the comments frequently." + ' ' +
+
+"Customer comments on cards" + ' ' +
+"We review the comments to learn how our users can get the most out of Trello. This information is really helpful! We may not get to respond to all of the comments on this board, but please keep the feedback coming." + ' ' +
+
+"The best place to get support isn’t on this board, but rather via help.trello.com. If you can't find what you're looking for, contact us. We can provide much better support via email than via public comments on this board." + ' ' +
+
+"We want cards to be a place where you can read the history of updates from the team without needing to try to figure out what’s a comment from the Trello team and what’s a comment from a customer. Since the primary purpose of public comments is an input stream of insights from our customers, we’ll be deleting public comments after seven days. This will cause comments from Trello team members to be much more visible. (We may not implement this immediately, but expect this change to happen eventually)."
+
+
                   ), 
                   React.createElement("div", {className: "activity"}, 
                      React.createElement("header", null, 
@@ -23379,7 +23395,7 @@ var CardPage = React.createClass({displayName: "CardPage",
                            React.createElement("p", null, "999 Comments")
                         )
                      ), 
-                     React.createElement("ul", null, 
+                     React.createElement("ul", {className: "comments"}, 
                         commentItems
                      )
 
--- src/js/components/pages/CardPage/CardPage.js
+++ src/js/components/pages/CardPage/CardPage.js
@@ -15,44 +15,46 @@ var CardPage = React.createClass({
       var comments = [
          {
             username: 'user-00',
-            avatarUrl: 'foo.png',
+            avatarUrl: '/img/avatar.png',
             commentText: "@hostilefork Interesting thought, I'll pass that on! For now, you might want to manually add a visual indicator to the card at the top of the list to make that more apparent(e.g. attach a pic and use as card cover)",
             timestamp: 'Aug 12 at 6:56 PM'
          },
          {
             username: 'user-00',
-            avatarUrl: 'foo.png',
+            avatarUrl: '/img/avatar.png',
             commentText: "@hostilefork Interesting thought, I'll pass that on! For now, you might want to manually add a visual indicator to the card at the top of the list to make that more apparent(e.g. attach a pic and use as card cover)",
             timestamp: 'Aug 12 at 6:56 PM'
          },
          {
             username: 'user-01',
-            avatarUrl: 'foo.png',
+            avatarUrl: '/img/avatar.png',
             commentText: "@hostilefork Interesting thought, I'll pass that on! For now, you might want to manually add a visual indicator to the card at the top of the list to make that more apparent(e.g. attach a pic and use as card cover)",
             timestamp: 'Aug 12 at 6:56 PM'
          },
          {
             username: 'user-02',
-            avatarUrl: 'foo.png',
+            avatarUrl: '/img/avatar.png',
             commentText: "@hostilefork Interesting thought, I'll pass that on! For now, you might want to manually add a visual indicator to the card at the top of the list to make that more apparent(e.g. attach a pic and use as card cover)",
             timestamp: 'Aug 12 at 6:56 PM'
          },
          {
             username: 'user-03',
-            avatarUrl: 'foo.png',
+            avatarUrl: '/img/avatar.png',
             commentText: "@hostilefork Interesting thought, I'll pass that on! For now, you might want to manually add a visual indicator to the card at the top of the list to make that more apparent(e.g. attach a pic and use as card cover)",
             timestamp: 'Aug 12 at 6:56 PM'
          }
       ];
 
       var commentItems = comments.map(c => (
-         <li>
-            <span className="username">{c.username}</span>
+         <li className="comment">
+            <h2 className="username">{c.username}</h2>
             <img className="avatar" src={c.avatarUrl} />
             <div className="content">
                {c.commentText}
             </div>
-            <time>{c.timestamp}</time>
+            <p className="meta">
+               <time className="timestamp">{c.timestamp}</time>
+            </p>
          </li>
       ));
 
@@ -62,16 +64,30 @@ var CardPage = React.createClass({
             <div className="wrap">
                <div className="main">
                   <header>
-                     <h1 className="title">Card Title</h1>
+                     <h1 className="title">What can you expect from this board</h1>
                      <div className="meta">
-                        <MetaItem icon="summary" className="list">
-                           Foo List
+                        <MetaItem icon="summary" className="parent-list">
+                           <a href="#">Foo List</a>
                         </MetaItem>
                      </div>
                   </header>
-                  <img className="cover" src="foo.png" />
+                  <img className="cover" src="/img/avatar.png" />
                   <div className="description">
                      <p>This is the description for card #{this.props.params.id}</p>
+                     Why is this board here?
+The primary purpose of this board is to communicate with our customers what the Trello team is working on, what’s potentially coming up, and to listen to the insights our customers have about what they’d like to see in Trello.
+
+Regular updates to cards that are In Progress
+If a card is “In Progress,” that means we’re working on it. You can expect to see regular updates to cards that are on the In Progress list in Trello. The Trello support team will update this list on a regular basis and read the comments frequently.
+
+Customer comments on cards
+We review the comments to learn how our users can get the most out of Trello. This information is really helpful! We may not get to respond to all of the comments on this board, but please keep the feedback coming.
+
+The best place to get support isn’t on this board, but rather via help.trello.com. If you can't find what you're looking for, contact us. We can provide much better support via email than via public comments on this board.
+
+We want cards to be a place where you can read the history of updates from the team without needing to try to figure out what’s a comment from the Trello team and what’s a comment from a customer. Since the primary purpose of public comments is an input stream of insights from our customers, we’ll be deleting public comments after seven days. This will cause comments from Trello team members to be much more visible. (We may not implement this immediately, but expect this change to happen eventually).
+
+
                   </div>
                   <div className="activity">
                      <header>
@@ -80,7 +96,7 @@ var CardPage = React.createClass({
                            <p>999 Comments</p>
                         </div>
                      </header>
-                     <ul>
+                     <ul className="comments">
                         {commentItems}
                      </ul>
 
